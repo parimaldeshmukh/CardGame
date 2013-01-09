9da4b41fee63e5b4c9cb292984f53268b8235862
Apurv <anerlekar@equalexperts.com>
2013-01-09 17:25:18 +0530

    added DeckBuilderSpecs

---
+++ test/ee/scala/cardGame/DeckBuilderSpecs.scala
@@ -0,0 +1,17 @@
+package ee.scala.cardGame
+
+import org.junit.{Assert, Test}
+
+class DeckBuilderSpecs {
+@Test
+  def itChecksForPopulatedDeck(){
+  //given
+  //when
+  val deck=new DeckBuilder buildDeck
+
+  //then
+   Assert assertEquals(deck.cards(0).suite, CardSuite.Diamond)
+   Assert assertEquals(deck.cards(0).value, CardFaceValue.Ace)
+
+}
+}
