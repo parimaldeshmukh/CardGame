3be4635d2cf52e593cb973acbb93b4f6c37b980b
Parimal Deshmukh <pdeshmukh@equalexperts.com>
2013-01-10 10:22:12 +0000

    minor refactorings to deck

--- src/ee/scala/cardGame/Deck.scala
+++ src/ee/scala/cardGame/Deck.scala
@@ -6,15 +6,16 @@ import util.Random
 class Deck(cards : ListBuffer[Card] ) {
 
   def shuffle(): Deck = {
-    val shuffledDeck = Random shuffle cards
-
-    if (satisfyConditionsOn(shuffledDeck)) return this else shuffle()
+    Random shuffle cards
+    if (satisfyRandomiseConditionsOn(cards)) this else shuffle
   }
 
-  private def satisfyConditionsOn(shuffledDeck: ListBuffer[Card]): Boolean = {
+  private def satisfyRandomiseConditionsOn(shuffledCards: ListBuffer[Card]): Boolean = {
     (1 to 49) foreach {
       index =>
-        if (shuffledDeck(index).suite equals shuffledDeck(index + 1).suite equals shuffledDeck(index + 2).suite) return false
+        if (shuffledCards(index).suite equals shuffledCards(index + 1).suite equals
+          shuffledCards(index + 2).suite)
+          return false
     }
     true
   }
