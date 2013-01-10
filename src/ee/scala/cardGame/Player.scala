e5dfde4912c90327f58b81b1fd4545600142f7f0
Parimal Deshmukh <pdeshmukh@equalexperts.com>
2013-01-10 11:21:23 +0000

    refactored story 2 - playerspecs, commenting out CardGame

--- src/ee/scala/cardGame/Player.scala
+++ src/ee/scala/cardGame/Player.scala
@@ -3,15 +3,15 @@ package ee.scala.cardGame
 import collection.mutable.ListBuffer
 
 class Player {
-  val cards: ListBuffer[Card] = new ListBuffer
+  val cardsInHand: ListBuffer[Card] = new ListBuffer
   var winCount = 0
   var round = 0
 
-  def acceptCard(card: Card) {
-    cards append (card)
+  def showCard() : Card = {
+    cardsInHand.remove(0)
   }
 
-  def drawCard(): Card = {
-    cards(round)
+  def drawCardFrom(deck : Deck) = {
+    cardsInHand.+=(deck.drawCard)
   }
 }
