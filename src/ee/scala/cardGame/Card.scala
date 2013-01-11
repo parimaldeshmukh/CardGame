cb78ff44e425ddbdfb1aef61db0bac1a815107e7
Parimal Deshmukh <pdeshmukh@equalexperts.com>
2013-01-11 10:36:50 +0000

    made Card pretty

--- src/ee/scala/cardGame/Card.scala
+++ src/ee/scala/cardGame/Card.scala
@@ -3,8 +3,8 @@ package ee.scala.cardGame
 class Card(suite: CardSuite.Value, face: CardFace.Value) {
 
   def compareTo(other: Card):Int = {
-    val suiteComparison : Int = this.suite.compareTo(other.getSuite())
-    if (suiteComparison!=0) suiteComparison else this.face.compareTo(other.getFace())
+    val suiteComparison = suite compareTo(other getSuite)
+    if (suiteComparison!=0) suiteComparison else face compareTo(other getFace)
   }
 
   def getSuite() : CardSuite.Value = {
